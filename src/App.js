29c5c7a03fa73cc0f65cee016ff2ac9fdbdff44f
ravibatchala <batchalaraviteja@gmail.com>
2021-07-14 20:48:09 +0530

    created public and private routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import 'rsuite/dist/styles/rsuite-default.css';
+import { Switch } from 'react-router';
+import Home from './pages/Home';
+import SignIn from './pages/SignIn';
 import './styles/main.scss';
+import PrivateRoute from './components/PrivateRoute';
+import PublicRoute from './components/PublicRoute';
 // import { Button } from 'rsuite';
 
 function App() {
-  return <div>Chat App</div>;
+  return (
+    <Switch>
+      <PublicRoute path="/signin">
+        <SignIn />
+      </PublicRoute>
+      <PrivateRoute path="/">
+        <Home />
+      </PrivateRoute>
+    </Switch>
+  );
 }
 
 export default App;
