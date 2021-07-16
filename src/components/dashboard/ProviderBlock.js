62df25716f8e2a9c26a7c9d2adfe56dcd42d9542
ravibatchala <batchalaraviteja@gmail.com>
2021-07-16 19:59:14 +0530

    linked facebook and google to one account, created provider block

---
+++ src/components/dashboard/ProviderBlock.js
@@ -0,0 +1,94 @@
+import React, { useState } from 'react';
+import { Alert, Button, Icon, Tag } from 'rsuite';
+import firebase from 'firebase/app';
+import { auth } from '../../misc/firebase';
+
+const ProviderBlock = () => {
+  const [isConnected, setIsConnected] = useState({
+    'google.com': auth.currentUser.providerData.some(
+      data => data.providerId === 'google.com'
+    ),
+    'facebook.com': auth.currentUser.providerData.some(
+      data => data.providerId === 'facebook.com'
+    ),
+  });
+
+  const updateIsConnected = (providerId, value) => {
+    setIsConnected(p => {
+      return {
+        ...p,
+        [providerId]: value,
+      };
+    });
+  };
+
+  const unlink = async providerId => {
+    try {
+      if (auth.currentUser.providerData.length === 1) {
+        throw new Error(`You cannot disconnect from ${providerId}`);
+      }
+      await auth.currentUser.unlink(providerId);
+      updateIsConnected(providerId, false);
+      Alert.info(`Disconnected from ${providerId}`, 4000);
+    } catch (err) {
+      Alert.error(err.message, 4000);
+    }
+  };
+  const unlinkGoogle = () => {
+    unlink('google.com');
+  };
+  const unlinkFacebook = () => {
+    unlink('facebook.com');
+  };
+
+  const link = async provider => {
+    try {
+      await auth.currentUser.linkWithPopup(provider);
+      updateIsConnected(provider.providerId, true);
+      Alert.info(`Linked to ${provider.providerId}`, 4000);
+    } catch (err) {
+      Alert.error(err.message, 4000);
+    }
+  };
+  const linkGoogle = () => {
+    link(new firebase.auth.GoogleAuthProvider());
+  };
+  const linkFacebook = () => {
+    link(new firebase.auth.FacebookAuthProvider());
+  };
+
+  return (
+    <div>
+      {isConnected['google.com'] && (
+        <Tag color="green" closable onClose={unlinkGoogle}>
+          <Icon icon="google" />
+          <b /> Connected
+        </Tag>
+      )}
+
+      {isConnected['facebook.com'] && (
+        <Tag color="blue" closable onClose={unlinkFacebook}>
+          <Icon icon="facebook" />
+          <b /> Connected
+        </Tag>
+      )}
+
+      <div className="mt-2">
+        {!isConnected['google.com'] && (
+          <Button block color="green" onClick={linkGoogle}>
+            <Icon icon="google" />
+            <b /> Link to Google
+          </Button>
+        )}
+        {!isConnected['facebook.com'] && (
+          <Button block color="blue" onClick={linkFacebook}>
+            <Icon icon="facebook" />
+            <b /> Link to Facebook
+          </Button>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default ProviderBlock;
