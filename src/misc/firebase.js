d2cef9dc4879f1d85a0dc930ebc7b814e4fa4a00
ravibatchala <batchalaraviteja@gmail.com>
2021-07-17 20:31:00 +0530

    completed displaying user avatar/initials

--- src/misc/firebase.js
+++ src/misc/firebase.js
@@ -1,6 +1,7 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/database';
+import 'firebase/storage';
 
 const config = {
   apiKey: 'AIzaSyDuHqJTs-ZIfJxor3t7CMvoPH3ZixWSKFU',
@@ -14,3 +15,4 @@ const config = {
 const app = firebase.initializeApp(config);
 export const auth = app.auth();
 export const database = app.database();
+export const storage = app.storage();
