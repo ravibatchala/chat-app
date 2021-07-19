0e745f218b9af96dc6ea2fe02f67e959802b211d
ravibatchala <batchalaraviteja@gmail.com>
2021-07-19 20:45:43 +0530

    update all user references if data is update (Denormalizing the data)

--- src/components/dashboard/index.js
+++ src/components/dashboard/index.js
@@ -1,30 +1,38 @@
 import React from 'react';
-import { Button, Drawer, Divider, Alert } from 'rsuite';
+import { Drawer, Button, Divider, Alert } from 'rsuite';
 import { useProfile } from '../../context/profile.context';
-import { database } from '../../misc/firebase';
 import EditableInput from '../EditableInput';
-import AvatarUploadBtn from './AvatarUploadBtn';
+import { database } from '../../misc/firebase';
 import ProviderBlock from './ProviderBlock';
+import AvatarUploadBtn from './AvatarUploadBtn';
+import { getUserUpdates } from '../../misc/helpers';
 
 const Dashboard = ({ onSignOut }) => {
   const { profile } = useProfile();
-  const onSave = async newData => {
-    const userNicknameRef = database
-      .ref(`/profiles/${profile.uid}`)
-      .child('name');
 
+  const onSave = async newData => {
     try {
-      await userNicknameRef.set(newData);
-      Alert.success('Nickname has been updated');
+      const updates = await getUserUpdates(
+        profile.uid,
+        'name',
+        newData,
+        database
+      );
+
+      await database.ref().update(updates);
+
+      Alert.success('Nickname has been updated', 4000);
     } catch (err) {
       Alert.error(err.message, 4000);
     }
   };
+
   return (
     <>
       <Drawer.Header>
         <Drawer.Title>Dashboard</Drawer.Title>
       </Drawer.Header>
+
       <Drawer.Body>
         <h3>Hey, {profile.name}</h3>
         <ProviderBlock />
@@ -37,9 +45,10 @@ const Dashboard = ({ onSignOut }) => {
         />
         <AvatarUploadBtn />
       </Drawer.Body>
+
       <Drawer.Footer>
         <Button block color="red" onClick={onSignOut}>
-          Sign Out
+          Sign out
         </Button>
       </Drawer.Footer>
     </>
