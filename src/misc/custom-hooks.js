07cf604853da872fabe6d6d1619c4740ccd76df6
ravibatchala <batchalaraviteja@gmail.com>
2021-07-20 17:09:21 +0530

    displayed real time presence of user

--- src/misc/custom-hooks.js
+++ src/misc/custom-hooks.js
@@ -1,4 +1,5 @@
-import { useCallback, useEffect, useState } from 'react';
+import { useState, useCallback, useEffect } from 'react';
+import { database } from './firebase';
 
 export function useModalState(defaultValue = false) {
   const [isOpen, setIsOpen] = useState(defaultValue);
@@ -8,9 +9,6 @@ export function useModalState(defaultValue = false) {
 
   return { isOpen, open, close };
 }
-
-// usage
-// const is992px = useMediaQuery('(max-width: 992px)')
 
 export const useMediaQuery = query => {
   const [matches, setMatches] = useState(
@@ -29,3 +27,25 @@ export const useMediaQuery = query => {
 
   return matches;
 };
+
+export function usePresence(uid) {
+  const [presence, setPresence] = useState(null);
+
+  useEffect(() => {
+    const userStatusRef = database.ref(`/status/${uid}`);
+
+    userStatusRef.on('value', snap => {
+      if (snap.exists()) {
+        const data = snap.val();
+
+        setPresence(data);
+      }
+    });
+
+    return () => {
+      userStatusRef.off();
+    };
+  }, [uid]);
+
+  return presence;
+}
