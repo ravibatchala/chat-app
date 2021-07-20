da8703d1ac73ee03c4cb89955dbf2919c40ab114
ravibatchala <batchalaraviteja@gmail.com>
2021-07-20 16:50:16 +0530

    added real time presence of the users

--- src/components/dashboard/DashboardToggle.js
+++ src/components/dashboard/DashboardToggle.js
@@ -1,23 +1,32 @@
 import React, { useCallback } from 'react';
-import { Alert, Button, Drawer, Icon } from 'rsuite';
+import { Button, Icon, Drawer, Alert } from 'rsuite';
+import { useModalState, useMediaQuery } from '../../misc/custom-hooks';
 import Dashboard from '.';
-import { useMediaQuery, useModalState } from '../../misc/custom-hooks';
-import { auth } from '../../misc/firebase';
+import { auth, database } from '../../misc/firebase';
+import { isOfflineForDatabase } from '../../context/profile.context';
 
 const DashboardToggle = () => {
-  const { isOpen, open, close } = useModalState();
+  const { isOpen, close, open } = useModalState();
   const isMobile = useMediaQuery('(max-width: 992px)');
 
   const onSignOut = useCallback(() => {
-    auth.signOut();
-    Alert.info('Signed Out Successfully', 4000);
-    close();
+    database
+      .ref(`/status/${auth.currentUser.uid}`)
+      .set(isOfflineForDatabase)
+      .then(() => {
+        auth.signOut();
+        Alert.info('Signed out', 4000);
+        close();
+      })
+      .catch(err => {
+        Alert.error(err.message, 4000);
+      });
   }, [close]);
+
   return (
     <>
       <Button block color="blue" onClick={open}>
-        <Icon icon="dashboard" />
-        Dashboard
+        <Icon icon="dashboard" /> Dashboard
       </Button>
       <Drawer full={isMobile} show={isOpen} onHide={close} placement="left">
         <Dashboard onSignOut={onSignOut} />
